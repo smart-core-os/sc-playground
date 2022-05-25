b4a6d6245a6c112d54196086a76471de46255cc9
Matt Nathan <matt.nathan@vanti.co.uk>
2022-05-25 11:16:07 +0100

    build: try to fix missing files in go:embed

--- ui/embed.go
+++ ui/embed.go
@@ -7,7 +7,7 @@ import (
 	"io/fs"
 )
 
-//go:embed all:playground/dist
+//go:embed playground/dist
 var playground embed.FS
 var Playground, _ = fs.Sub(playground, "playground/dist/embed")
 var NotFound, _ = fs.ReadFile(playground, "playground/dist/404.html")
